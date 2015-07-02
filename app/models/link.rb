42d415ae0cea2c2404f00ea94a20c3a696c71a35
Grant George <grantelliottgeorge@gmail.com>
2015-07-02 11:40:59 -0400

    fixups

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,12 +6,14 @@ class Link < ActiveRecord::Base
   def create_pdf
     Rails.logger.debug self.inspect.light_red
 
+    save_name = Time.now.to_i.to_s + ".pdf"
+
     kit = PDFKit.new(self.url, page_size: "Letter",
                                disable_smart_shrinking: true,
                                disable_external_links: true,
                                disable_internal_links: true,
                                print_media_type: true)
     pdf = kit.to_pdf
-    file = kit.to_file(File.expand_path('/Users/g/Desktop/pdfs/yolo.pdf'))
+    file = kit.to_file(Rails.root.join("public/pdfs/#{save_name}"));
   end
 end
---
+++ app/services/pdf_service.rb
@@ -0,0 +1,9 @@
+class PdfService
+  def initialize(user)
+    @current_user = user
+  end
+
+  def make_pdf_for_link(link)
+
+  end
+end
