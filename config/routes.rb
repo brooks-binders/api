475394df8a5a2783f2151fac040c7fd370483b01
Grant George <grantelliottgeorge@gmail.com>
2015-07-02 00:30:19 -0400

    add angular

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  # root :to => "home#index"
 
   namespace :v1, defaults: { format: :json } do
     resource :login, only: [:create], controller: :sessions
