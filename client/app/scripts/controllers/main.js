c5fcafe57e33750570872abe8bf0db671d816279
Grant George <grantelliottgeorge@gmail.com>
2015-07-03 20:23:29 -0400

    try grunt'

---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clipbooksApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the clipbooksApp
+ */
+angular.module('clipbooksApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
